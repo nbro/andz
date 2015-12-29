55d4178e3b6e97b9712caa076be8be56500aa053
dossan <nelson.brochado@usi.ch>
2015-12-29 04:00:05 +0100

    Adding the greedy algorithms I had created

---
+++ algorithms/greedy/activity_selection.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Author: Nelson Brochado
+
+Consider a set of requests for a room.
+
+Only one person can reserve the room at a time,
+and you want to allow the maximum number of requests.
+
+The requests for periods (si=start time for i, fi=finish time for i) are:
+
+(1, 4), (3, 5), (0, 6), (5, 7), (3, 8), (5, 9), (6, 10), (8, 11), (8, 12), (2, 13), (12, 14)
+
+Where for example in (1, 4), s1 = 1 and f1 = 4.
+
+Which ones should we schedule?
+
+We can solve this problem using dynamic programming,
+but it we can also solve it using a simple greedy algorithm.
+
+The algorithm consists of basically choosing the next activity
+with the smallest finish time.
+So, to do this, we need first to sort the activities by finish time.
+This algorithm is a top-down algorithm,
+in the sense that we can start by choosing an activity,
+the one with the earliest finish time,
+and then we can do the same for the remaining sub-problems.
+"""
+
+import operator
+from tabulate import tabulate
+
+
+activities = [["a", 12, 14], ["b", 0, 6], ["c", 2, 13], ["d", 3, 5],
+              ["e", 5, 7], ["f", 1, 4], ["g", 5, 9], ["h", 3, 8],
+              ["i", 12, 14], ["j", 6, 10], ["k", 8, 11]]
+
+
+def ask_activities():
+    
+    print("Welcome to the Activity Selection problem!\n\n" +
+          "You tell me your activities and their starting\n" +
+          "and ending times, and I will tell you which\n" +
+          "activities are compatible, and should take place.")
+
+    print("=" * 48, end="\n\n")
+
+    activities.clear()
+    
+    while True:
+        name = input("Enter name of the activity: ")
+        starting_time = int(input("Enter the starting time of " + name + ": "))
+        ending_time = int(input("Enter the ending time of " + name + ": "))
+        activities.append([name, starting_time, ending_time])
+
+        a = input("\nType q if you don't have more activities: ")
+        
+        if a.lower() == "q":
+            break
+        else:
+            print("-" * 48, end="\n\n")
+
+
+def activity_selector(activities, verbose=True):
+    # sorting activities by finish time
+    activities.sort(key=operator.itemgetter(2))
+
+    if verbose:
+        print("\nAll activities ordered by finish time:")
+        print(tabulate(activities,
+                       headers=("Activity's Name", "Starting Time", "Ending Time"),
+                       tablefmt="grid"))
+
+    last_selected_activity = activities[0]
+
+    selected_activities = [activities[0]]
+
+    for i in range(1, len(activities)):
+        # if the starting time of the ith activity
+        # is greater or equal to the ending time
+        # of the last selected activity
+        # then add the ith activity to the selected activities.
+        if activities[i][1] >= last_selected_activity[2]:
+            selected_activities.append(activities[i])
+            last_selected_activity = activities[i]
+
+    if verbose:
+        print("\n\nYou should schedule your activities in the following way:")
+        print(tabulate(selected_activities,
+                       headers=("Activity's Name", "Starting Time", "Ending Time"),
+                       tablefmt="grid"))
+
+    return selected_activities
+
+
+if __name__ == "__main__":
+    # ask_activities()  # uncomment this line if you want to choose your activities manually
+    sa = activity_selector(activities)
+    print(sa)
---
+++ algorithms/greedy/fractional_knapsack.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Author: Nelson Brochado
+
+The time complexity of the fractional knapsack is O(n*log(n)),
+because of the call to sort the items by value/weight ratio.
+"""
+
+
+import operator
+from tabulate import tabulate
+
+
+def ask_objects():
+    objects = []
+
+    print("Welcome to the Fractional Knapsack problem!\n\n" +
+          "You will tell me the objects that you have,\n" +
+          "their cost and their weight.\n\n" +
+          "You should also tell me after that\n"
+          "how much weight you can carry with you.\n\n" +
+          "I will tell you then which items or\n" +
+          "fraction of items you should take.\n")
+
+    input("When you are ready, press ENTER.\n" + "=" * 40 + "\n\n")
+
+    while True:
+        name = input("Enter the name of the object: ")
+        cost = int(input("Enter the value of " + name + ": "))
+        weight = int(input("Enter the weight (in grams) of " + name + ": "))
+        objects.append([name, cost, weight])
+
+        yn = input("\nDo you have other items (y/n)? ")
+
+        if yn.lower() in ("n", "no"):
+            break
+        else:
+            print("-" * 40, end="\n\n")
+
+    for obj in objects:
+        # adding as forth property of each object its cost/weight ratio
+        obj.append(obj[1] / obj[2])
+
+    objects.sort(key=operator.itemgetter(3), reverse=True)
+
+    print("\n\nThe following are the items that you have:\n")
+    print(tabulate(objects, tablefmt="grid", headers=("Name", "Value", "Weight", "Value/Weight Ratio")))
+
+    capacity = int(input("\nEnter the maximum weight you can bring (in grams): "))
+
+    return objects, capacity
+
+
+def interactive_fractional_knapsack():
+
+    objects, capacity = ask_objects()
+
+    current_weight = 0
+
+    knapsack_objects = []
+
+    for i, obj in enumerate(objects):
+        if obj[2] + current_weight <= capacity:
+            current_weight += obj[2]
+            knapsack_objects.append(i)
+        else:
+            remaining_weight = capacity - current_weight
+            knapsack_objects.append((i, remaining_weight))
+            break
+
+    output_fractional_knapsack(knapsack_objects, objects)
+
+
+def output_fractional_knapsack(knapsack_objects, objects):
+    s = "You should take "
+
+    for i, item in enumerate(knapsack_objects):
+        if not isinstance(item, tuple):
+            s += str(objects[item][2]) + " gram(s) of " + objects[item][0]
+            if i < len(knapsack_objects) - 1:
+                s += ", "
+        else:
+            s += " and " + str(item[1]) + " gram(s) of " + objects[item[0]][0] + "."
+
+    print("\n\n" + s)
+
+
+if __name__ == "__main__":
+    interactive_fractional_knapsack()
