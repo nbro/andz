cc1b5f2aa0de8f8e589c1f061589b2fad309d161
Nelson Brochado <nelson.brochado@outlook.com>
2016-11-18 14:18:36 +0100

    Added more explanations based on the slides from the course ands2 by prof. Papadopoulou

--- ands/algorithms/dp/longest_common_subsequence.py
+++ ands/algorithms/dp/longest_common_subsequence.py
@@ -12,6 +12,10 @@ where characters are not necessarily contiguous?
 You can find a recursive and a two dynamic programming implementations for the LCS problem.
 You can find just one implementation using dynamic programming that actually returns the LCS,
 instead of just computing its length, like all other implementations do.
+
+## References
+- Introduction to Algorithms (3rd ed) by CLRS
+- Slides by prof. Evanthia Papadopoulou
 """
 
 from pprint import pprint
@@ -53,19 +57,7 @@ def _recursive_lcs_length_aux(s1, n, s2, m, result):
     elif s1[n - 1] == s2[m - 1]:
         result = 1 + _recursive_lcs_length_aux(s1, n - 1, s2, m - 1, result)
     else:
-        result = max(
-            _recursive_lcs_length_aux(
-                s1,
-                n - 1,
-                s2,
-                m,
-                result),
-            _recursive_lcs_length_aux(
-                s1,
-                n,
-                s2,
-                m - 1,
-                result))
+        result = max(_recursive_lcs_length_aux(s1, n - 1, s2, m, result), _recursive_lcs_length_aux(s1, n, s2, m - 1, result))
     return result
 
 
@@ -74,10 +66,56 @@ def recursive_lcs_length(s1, s2):
 
     This algorithm uses a recursive solution,
     as the name suggests, but this results in an exponential algorithm.
+    
+    ### Idea
+    For every subsequence of s1 check weather it's a subsequence of s2.
+    There are 2^n subsequences of s1 to check.
+    Each subsequence takes O(m) time to check:
+    scan s2 for the first letter, from there scan for the second, and so on.
+    
+    ### Definition
+    LCS(i, j) = length of the longest comment subsequence between
+    s1(i) = s1(i)_1, s1(i)_2, ..., s1(i)_i and s2(j) = s2(j)_1, s2(j)_2, ..., s2(j)_j.
+    
+    ### Goal 
+    LCS(n, m), where n = length(s1) and m = length(s2).
+    
+    ### Algorithm  
+    In the following descriptions s1(i) means a subsequence of s1 up to i.
+    Instead, s1(i)_i means the ith element of that same subsequence s1(i).
+    
+    If s1(i) or s2(j) is empty, LCS(i, j) = 0.
+    
+    If s1(i)_i == s2(j)_j, then s1(i)_i and s2(j)_j must be included in the LCS(i, j),
+    and we express it as LCS(i, j) = LCS(i - 1, j - 1) + 1,
+    where the +1 stands for the inclusion of s1(i)_i and s2(j)_j.
+    
+    If s1(i)_i != s2(j)_j, then we can either skip s1(1)_i or s2(1)_j (or both):
+    we need to choose the best. So lets see these options more closely.
+    Option 1: s1(i)_i is not in the LCS, then LCS(i, j) = LCS(i - 1, j).
+    Option 2: s2(j)_j is not in the LCS, then LCS(i, j) = LCS(i, j - 1).
+    So, basically, what we do is: LCS(i, j) = max(LCS(i - 1, j), LCS(i, j - 1)).
+    Note that we don't really need to include LCS(i - 1, j - 1), 
+    for the case where neither s1(i)_i nor s2(j)_j are included in the LCS(i, j),
+    because max(LCS(i - 1, j), LCS(i, j - 1), LCS(i - 1, j - 1)) = max(LCS(i - 1, j), LCS(i, j - 1)),
+    i.e. the maximum "profit" can simply be retrieved from LCS(i - 1, j) and LCS(i, j - 1),
+    since for sure LCS(i - 1, j - 1) brings less (or equal) profit than LCS(i - 1, j) or LCS(i, j - 1).
+    
+    ### Summary
+                +--
+                | 0                                   if i == 0 or j == 0.
+    LCS(i, j) = | LCS(i - 1, j - 1) + 1               if i, j > 0 and s1(i)_i == s2(j)_j.
+                | max(LCS(i - 1, j), LCS(i, j - 1))   if i, j > 0 and s1(i)_i != s2(j)_j.
+                +--
+                
+    ### Complexity
+    This plain recursive approach is very inefficient, 
+    because we keep on recomputing sub-problems.
+    
+    Time complexity: theta(m*2^n).    
 
     :type s1 : str
     :type s2 : str"""
-
     n = len(s1)
     m = len(s2)
     result = 0
@@ -99,9 +137,7 @@ def _memoized_recursive_lcs_length_aux(s1, n, s2, m, result, matrix):
     elif matrix[n - 1][m - 1] is not None:
         return matrix[n - 1][m - 1]
     elif s1[n - 1] == s2[m - 1]:
-        result = 1 + \
-            _memoized_recursive_lcs_length_aux(
-                s1, n - 1, s2, m - 1, result, matrix)
+        result = 1 + _memoized_recursive_lcs_length_aux(s1, n - 1, s2, m - 1, result, matrix)
     else:
         result = max(_memoized_recursive_lcs_length_aux(s1, n - 1, s2, m, result, matrix),
                      _memoized_recursive_lcs_length_aux(s1, n, s2, m - 1, result, matrix))
@@ -114,8 +150,7 @@ def _memoized_recursive_lcs_length_aux(s1, n, s2, m, result, matrix):
 def memoized_recursive_lcs_length(s1, s2):
     """Returns the length of the LCS between s1 and s2.
 
-    This algorithm uses memoization
-    to improve performance with respect to recursive_lcs_length.
+    This algorithm uses memoization to improve performance with respect to recursive_lcs_length.
 
     The running time complexity of this algorithm
     should be O(len(s1) * len(s2)),
@@ -134,10 +169,8 @@ def memoized_recursive_lcs_length(s1, s2):
 
 
 def bottom_up_lcs_length(s1, s2, matrix=False):
-    """Returns the length of the LCS between s1 and s2,
-    if matrix is not set to True,
-    else it returns the matrix used
-    to calculate the length of the LCS of sub-problems.
+    """Returns the length of the LCS between s1 and s2, if matrix is not set to True,
+    else it returns the matrix used to calculate the length of the LCS of sub-problems.
 
     :type s1 : str
     :type s2 : str
