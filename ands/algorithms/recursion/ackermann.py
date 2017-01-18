ee59ae70bf5537100d15155ddb05bce414ea5d54
nelson-brochado <nelson.brochado@outlook.com>
2017-01-18 13:49:39 +0100

    Adding tests for the crazy Ackermann function

--- ands/algorithms/recursion/ackermann.py
+++ ands/algorithms/recursion/ackermann.py
@@ -2,33 +2,39 @@
 # -*- coding: utf-8 -*-
 
 """
+## Meta info
+
 Author: Nelson Brochado
 
 Creation: 22/02/16
 
-Ackermann function grows faster than an exponential function,
-or even a multiple exponential function.
-So, do not even try to run this function!
+Updated: 18/01/2017
+
+## Description
+
+The Ackermann function is the simplest example of a well defined total function.
+Total function means that it's defined for all possible inputs.
+The function is computable, but **not** primitive recursive.
+A primitive recursive function is a function that can be implemented using only "for" loops,
+i.e. loops that have a fixed number of iterations.
+A computable function is a function that can be implemented using "while" loops.
+Note that "do" loops are a particular case of while loops.
+
+It grows faster than an exponential function, or even a multiple exponential function.
 
 ## References
 
-- http://mathworld.wolfram.com/AckermannFunction.html
-
+- [http://mathworld.wolfram.com/AckermannFunction.html](http://mathworld.wolfram.com/AckermannFunction.html)
+- [http://math.stackexchange.com/questions/75296/what-is-the-difference-between-total-recursive-and-primitive-recursive-functions](http://math.stackexchange.com/questions/75296/what-is-the-difference-between-total-recursive-and-primitive-recursive-functions)
+- [https://en.wikipedia.org/wiki/Ackermann_function](https://en.wikipedia.org/wiki/Ackermann_function)
 """
 
 
-def ackermann(x, y):
-    if x == 0:
-        return y + 1
-    elif y == 0:
-        return ackermann(x - 1, 1)
+def ackermann(m, n):
+    assert m >= 0 and n >= 0
+    if m == 0:
+        return n + 1
+    elif n == 0:
+        return ackermann(m - 1, 1)
     else:
-        return ackermann(x - 1, ackermann(x, y - 1))
-
-
-if __name__ == "__main__":
-    print(ackermann(2, 3))
-    try:
-        print(ackermann(3, 10))
-    except RuntimeError:
-        pass
+        return ackermann(m - 1, ackermann(m, n - 1))
---
+++ tests/algorithms/recursion/test_ackermann.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Author: Nelson Brochado
+
+Created: 18/01/2017
+
+Testing the first 3*3 = 9 permutations for two inputs m and n from a domain {0, 1, 2}
+"""
+
+import unittest
+
+from ands.algorithms.recursion.ackermann import ackermann
+
+
+class TestAckermann(unittest.TestCase):
+    def test_m0_n0(self):
+        self.assertEqual(ackermann(0, 0), 1)
+
+    def test_m0_n1(self):
+        self.assertEqual(ackermann(0, 1), 2)
+
+    def test_m0_n2(self):
+        self.assertEqual(ackermann(0, 2), 3)
+
+    def test_m1_n0(self):
+        self.assertEqual(ackermann(1, 0), 2)
+
+    def test_m1_n1(self):
+        self.assertEqual(ackermann(1, 1), 3)
+
+    def test_m1_n2(self):
+        self.assertEqual(ackermann(1, 2), 4)
+
+    def test_m2_n0(self):
+        self.assertEqual(ackermann(2, 0), 3)
+
+    def test_m2_n1(self):
+        self.assertEqual(ackermann(2, 1), 5)
+
+    def test_m2_n2(self):
+        self.assertEqual(ackermann(2, 2), 7)
