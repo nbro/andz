3a0cf2046c8542aaafdb8f4b5f55635bd386b86e
Nelson Brochado <nelson.brochado@outlook.com>
2017-01-26 23:32:21 +0100

    Fixed a few things in 01-knapsack

--- ands/algorithms/dp/zero_one_knapsack.py
+++ ands/algorithms/dp/zero_one_knapsack.py
@@ -2,13 +2,29 @@
 # -*- coding: utf-8 -*-
 
 """
+## Meta info
+
 Author: Nelson Brochado
+Created: 2015
+Updated: 26/01/2017
+
+## Description
+
+Given n objects and a "knapsack". 
+Item i weighs w_i > 0 and has a value v_i > 0. 
+Knapsack has capacity W.
+Goal: fill knapsack so as to maximize total value.
+
+## References
+
+- Slides by prof. Evanthia Papadopoulou
+
 """
 
 from pprint import pprint
 
 
-def _get_zero_one_knapsack_matrix(total_weight, n):
+def _get_zero_one_knapsack_matrix(total_weight: int, n: int) -> list:
     """Returns a matrix for a dynamic programming solution to the 0/1 knapsack problem.
 
     The first row of this matrix contains the numbers
@@ -36,11 +52,10 @@ def _get_zero_one_knapsack_matrix(total_weight, n):
     return m
 
 
-def zero_one_knapsack_verbose(total_weight, weights, values):
+def zero_one_knapsack_verbose(total_weight: int, weights, values) -> int:
     """Verbose version of zero_one_knapsack."""
-    if len(weights) != len(values):
-        raise Exception(
-            "List of weights must have the same size as the list of values.")
+    assert len(weights) == len(values)
+    assert total_weight >= 0
 
     n = len(weights)
 
@@ -63,8 +78,7 @@ def zero_one_knapsack_verbose(total_weight, weights, values):
             if weights[i - 2] > w - 1:
                 profits[i][w] = profits[i - 1][w]
             else:
-                profits[i][w] = max(
-                    profits[i - 1][w], values[i - 2] + profits[i - 1][w - weights[i - 2]])
+                profits[i][w] = max(profits[i - 1][w], values[i - 2] + profits[i - 1][w - weights[i - 2]])
 
             print("Profits matrix after calculation:\n")
             pprint(profits)
@@ -73,29 +87,25 @@ def zero_one_knapsack_verbose(total_weight, weights, values):
     return profits[-1][-1]
 
 
-def zero_one_knapsack(total_weight, weights, values):
-    """Returns the maximum profit
-    that can be obtained by using items with weights and values,
-    and a total_weight.
+def zero_one_knapsack(total_weight: int, weights, values) -> int:
+    """Returns the maximum profit that can be obtained by 
+    using items with weights and values and a total_weight.
 
     This version does not tell which items to pick.
 
-    Running time complexity: O(n * total_weight),
-    where n is the number of items or len(items).
-    This is consider a pseudo-polynomial time algorithm.
+    **Time complexity**: O(n * total_weight), where n is the number of items.
+   
+    This is consider a pseudo-polynomial time algorithm, **not** polynomial!
 
-    This problem is NP-Complete,
+    The decision version of this problem is actually NP-Complete,
     and the running time complexity above does not contradict it:
     total_weight is not polynomial in the length of the input!
 
-    :type total_weight : int
     :type weights : list | tuple
     :type values : list | tuple
     """
-
-    if len(weights) != len(values):
-        raise Exception(
-            "List of weights must have the same size as the list of values.")
+    assert len(weights) == len(values)
+    assert total_weight >= 0
 
     n = len(weights)
 
@@ -109,39 +119,30 @@ def zero_one_knapsack(total_weight, weights, values):
 
             # If the weight of the (i - 2)th item is greater than w - 1,
             # which is the current weight being analysed.
-
-            # Note that the weights in the matrix profits are shifted to the
-            # right by 1.
+            # Note that the weights in the matrix `profits` are shifted to the right by 1.
             if weights[i - 2] > w - 1:
                 profits[i][w] = profits[i - 1][w]
-
-            # The weight of the current item is less than the total weight,
-            # but we need to decide if it is convenient to include this item or
-            # not.
             else:
-                # We need to decide if it is convenient to include the current item i - 2.
-                # (Note that indices are also shifted 2 positions to the bottom in the matrix "profits").
-                # To do this, we compare if we gain more by including it or not
-                # including it.
+                # Note: indices in the `profits` matrix are also shifted 2 positions to the bottom.
 
-                # "profits[i - 1][w]" refers to the profit of not including current item
-                # "values[i - 2]" refers to the value of the current item
-                # "w - weights[i - 2]" is the remaining weight, if we include the current item.
-                # Note that "weights[i - 2]" is the weight of the current item.
-                # "profits[i - 1][w - items[i - 2][1]]" refers to the profit obtained by
-                # an item with the remaining weight (not including the current
-                # item: i - 1)
-                profits[i][w] = max(
-                    profits[i - 1][w], values[i - 2] + profits[i - 1][w - weights[i - 2]])
+                # The weight of the current item is less (or equal) than the total weight,
+                # but we need to decide if it is convenient to include this item or not.
+                # To do this, we compare if we gain more by including it or not.
+
+                # `profits[i - 1][w]` refers to the profit of not including current item.
+                # `values[i - 2]` refers to the value of the current item.
+                # `w - weights[i - 2]` is the remaining weight, if we include the current item.
+                # Note: `weights[i - 2]` is the weight of the current item.
+                profits[i][w] = max(profits[i - 1][w], values[i - 2] + profits[i - 1][w - weights[i - 2]])
 
     return profits[-1][-1]
 
 
-def _recursive_01_knapsack_aux(capacity, w, v, value):
+def _recursive_01_knapsack_aux(capacity: int, w, v, value: int) -> int:
     """Either takes the last element or it doesn't.
 
     This algorithm takes exponential time.
-    """
+    """    
     if capacity == 0:
         return 0
     if len(w) > 0 and len(v) > 0:
@@ -153,14 +154,15 @@ def _recursive_01_knapsack_aux(capacity, w, v, value):
     return value
 
 
-def recursive_01_knapsack(total_weight, weights, values):
-    if len(weights) != len(values):
-        raise ValueError("len(weights) != len(values)")
+def recursive_01_knapsack(total_weight: int, weights, values):
+    assert len(weights) == len(values)
+    assert total_weight >= 0
+    
     value = 0
     return _recursive_01_knapsack_aux(total_weight, weights, values, value)
 
 
-def _memoized_01_knapsack_aux(capacity, w, v, value, m):
+def _memoized_01_knapsack_aux(capacity: int, w, v, value: int, m: list) -> int:
     """Either takes the last element or it doesn't.
 
     Memoization version of _recursive_01_knapsack_aux
@@ -185,7 +187,7 @@ def _memoized_01_knapsack_aux(capacity, w, v, value, m):
     return value
 
 
-def memoized_01_knapsack(capacity, weights, values):
+def memoized_01_knapsack(capacity: int, weights, values) -> int:
     result = 0
     m = [[None for _ in range(capacity)] for _ in range(len(weights))]
     return _memoized_01_knapsack_aux(capacity, weights, values, result, m)
