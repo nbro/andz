c9601d15410ff9a283b859980a3dcba0bec14f16
nelson-brochado <nelson.brochado@outlook.com>
2017-02-15 04:59:08 +0100

    Modularised the shell scripts

---
+++ scripts/colors.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Colors used when printing.
+GREEN=$(tput setaf 2)
+RED=$(tput setaf 1)
+NORMAL=$(tput sgr0)
+YELLOW=$(tput setaf 3)
--- scripts/new_docs.sh
+++ scripts/new_docs.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
-
-# Colors used when printing.
-GREEN=$(tput setaf 2)
-RED=$(tput setaf 1)
-NORMAL=$(tput sgr0)
-YELLOW=$(tput setaf 3)
+##############################################################################################
+# SCRIPT NAME: new_docs.sh                                                                   #
+#                                                                                            #
+# PURPOSE: create new documentation for the 'ands' module using the Python's 'pdoc' module   #
+#                                                                                            #
+# RUN FROM COMMAND LINE:  . ./new_docs.sh && new_docs                                        #
+##############################################################################################
 
 new_docs()
 {
-    printf "\n${YELLOW}Creating new documentation under './docs'...${NORMAL}\n"
+    printf "${YELLOW}Creating new documentation under '${PWD}/docs'...${NORMAL}\n"
     rm -rf ./docs
     mkdir docs
     pdoc --html --overwrite --html-dir docs ands
-    printf "${GREEN}Done.${NORMAL}\n\n"
+    printf "${GREEN}Done.${NORMAL}\n"
 }
+
+. ./_source_script.sh
+_source_script scripts colors
