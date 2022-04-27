b3f1f9181d39fe1ab886135d996b02141080fcb1
Nelson Brochado <nelson.brochado@outlook.com>
2022-04-28 00:09:41 +0200

    test_palindrome.py updated to test also iterative_is_palindrome

--- tests/algorithms/recursion/test_palindrome.py
+++ tests/algorithms/recursion/test_palindrome.py
@@ -8,7 +8,7 @@ Author: Nelson Brochado
 
 Created: 20/01/2017
 
-Updated: 04/08/2018
+Updated: 15/03/2022
 
 # Description
 
@@ -20,39 +20,50 @@ import string
 import unittest
 from random import choice, randint
 
-from ands.algorithms.recursion.palindrome import is_palindrome
+from ands.algorithms.recursion.palindrome import (is_palindrome,
+                                                  iterative_is_palindrome)
 
 
-class TestRecursivePalindrome(unittest.TestCase):
-    @staticmethod
-    def generate_palindrome(n: int):
-        """Generates a palindrome of size `n`"""
-        assert n > 0
-        if n == 1:
-            return choice(string.ascii_letters)
+def make_test(func: callable):
+    class TestIsPalindrome(unittest.TestCase):
+        @staticmethod
+        def generate_palindrome(n: int):
+            """Generates a palindrome of size `n`"""
+            assert n > 0
+            if n == 1:
+                return choice(string.ascii_letters)
 
-        p = []
-        for _ in range(n - 1):
-            char = choice(string.ascii_letters)
-            p.insert(len(p) // 2, char)
-            p.insert(len(p) // 2, char)
-        return "".join(p)
+            p = []
+            for _ in range(n - 1):
+                char = choice(string.ascii_letters)
+                p.insert(len(p) // 2, char)
+                p.insert(len(p) // 2, char)
+            return "".join(p)
 
-    def test_empty_str(self):
-        self.assertTrue(is_palindrome(""))
+        def test_empty_str(self):
+            self.assertTrue(func(""))
 
-    def test_size_1(self):
-        self.assertTrue(
-            is_palindrome(TestRecursivePalindrome.generate_palindrome(1)))
+        def test_size_1(self):
+            self.assertTrue(
+                func(TestIsPalindrome.generate_palindrome(1)))
 
-    def test_size_2(self):
-        self.assertTrue(
-            is_palindrome(TestRecursivePalindrome.generate_palindrome(2)))
+        def test_size_2(self):
+            self.assertTrue(
+                func(TestIsPalindrome.generate_palindrome(2)))
 
-    def test_size_2_not(self):
-        self.assertFalse(is_palindrome("xy"))
+        def test_size_2_not(self):
+            self.assertFalse(func("xy"))
 
-    def test_random_size(self):
-        n = randint(3, 100)
-        self.assertTrue(
-            is_palindrome(TestRecursivePalindrome.generate_palindrome(n)))
+        def test_random_size(self):
+            n = randint(3, 100)
+            self.assertTrue(
+                func(TestIsPalindrome.generate_palindrome(n)))
+
+    return TestIsPalindrome
+
+
+test_is_palindrome_recursively = make_test(is_palindrome)
+test_is_palindrome_iteratively = make_test(iterative_is_palindrome)
+
+if __name__ == '__main__':
+    unittest.main()
