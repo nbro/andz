a5af02802d1653a114de754050d81159dbab60e5
Nelson Brochado <nelson.brochado@outlook.com>
2022-04-28 01:04:48 +0200

    docstrings of reverse.py updated

--- ands/algorithms/recursion/reverse.py
+++ ands/algorithms/recursion/reverse.py
@@ -8,13 +8,17 @@ Author: Nelson Brochado
 
 Created: 22/07/2015
 
-Updated: 19/09/2017
+Updated: 15/03/2022
 
 # Description
 
 Reverses in-place the elements of a list using recursion.
 
 This method could also be adapted to work with other mutable collections.
+
+# TODO
+
+- Implement the iterative versions with slicing, i.e. s[::-1], and without it.
 """
 
 __all__ = ["reverse"]
