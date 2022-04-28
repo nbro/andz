2245d6ae712670c0c644cb787465e25774bada5a
Nelson Brochado <nelson.brochado@outlook.com>
2022-04-28 17:15:56 +0200

    counting_sort.py refactored to handle both implementations of counting-sort with a single function

--- ands/algorithms/sorting/integer/counting_sort.py
+++ ands/algorithms/sorting/integer/counting_sort.py
@@ -8,7 +8,7 @@ Author: Nelson Brochado
 
 Created: 03/03/2022
 
-Updated: 05/03/2022
+Updated: 07/03/2022
 
 # Description
 
@@ -21,7 +21,7 @@ of elements less than x. It uses this information to place element x directly
 into its position in the output list. For example, if 17 elements are less
 than x, then x should be placed in position 18 in the sorted list.
 
-Counting sort assumes that each of the n = a.length elements in a is an
+Counting sort assumes that each of the n = len(a) elements in a is an
 integer in the range 0 to k - 1, for some integer k, which is the largest
 possible number - 1 (although some descriptions and implementations assume
 that k is the largest possible) in a. However, note that these integers,
@@ -91,22 +91,11 @@ x = 1 will appear before the second 2 at index x = 3 in the sorted list b.
 
 The stability of a sorting algorithm can be important for multiple reasons.
 For example, let's say that we want to sort the array
-[(1, "what's up?"), (0, "ok"), (1, "nice")]. If we sort this array according to
-the first elements of the tuples, then (1, "what's up?") would still come
-before (1, "nice") in the sorted list, i.e. the sorted list would be
-[(0, "ok"), (1, "what's up?"), (1, "nice")] and not
-[(0, "ok"), (1, "nice"), (1, "what's up?")]. If this property is important,
-then counting sort can be useful.
-See https://stackoverflow.com/q/1517793/3924118 and
-https://en.wikipedia.org/wiki/Sorting_algorithm#Stability for more info.
-
-## Applications
-
-- Counting sort is often used as a subroutine in radix sort.
-    - The stability of counting sort is important in order for radix sort to
-    work correctly.
-- sort strings by the first (second, third, etc.) letter
-- sort phone numbers by area code
+[(1, "no"), (0, "ok"), (1, "yes")]. If we sort this array according to
+the first elements of the tuples, then (1, "no") would still come before
+(1, "yes") in the sorted list, i.e. the sorted list would be
+[(0, "ok"), (1, "no"), (1, "yes")] and not [(0, "ok"), (1, "yes"), (1, "no")].
+If this property is important, then counting sort can be useful.
 
 ## Example
 
@@ -235,6 +224,14 @@ The final sorted list b and the counter are
 
     c = [0, 0, 1, 1, 3]
 
+# Applications
+
+- Counting sort is often used as a subroutine in radix sort.
+    - The stability of counting sort is important in order for radix sort to
+    work correctly.
+- sort strings by the first (second, third, etc.) letter
+- sort phone numbers by area code
+
 # Invention
 
 Counting sort was invented by Harold H. Seward in 1954 in the paper
@@ -244,18 +241,13 @@ Sort", according to Donald Knuth.
 
 # Terminology
 
-Counting sort is also called "key-indexed counting" by Robert Sedgewick and
+- Counting sort is also called "key-indexed counting" by Robert Sedgewick and
 Kevin Wayne in their book "Algorithms" (4th edition), section 5.1, page 703,
 but it's in the context of sorting strings.
 
-# TODO
-
-- Add in-place version of counting sort, but note that this version is not
-stable. We could also copy b into a, once sorted, if we want to modify a
-in-place. This would still be a stable algorithm, but we would need an extra
-loop at the end.
-
-- Implement version where keys have associated values
+- k is also called "radix"; this terminology will clarify why radix-sort,
+which uses counting sort as a subroutine, is called radix-sort. In Sedgewick
+and Wayne's book, k is denoted by R to remind us that it refers to the radix.
 
 # References
 
@@ -268,15 +260,18 @@ Kevin Wayne
 lesson on "key-indexed counting" by Robert Sedgewick.
 - https://courses.csail.mit.edu/6.006/spring11/rec/rec11.pdf
 - https://en.wikipedia.org/wiki/Counting_sort
+- https://www.ime.usp.br/~yoshi/Sedgewick/Algs4th/Slides/51StringSorts.pdf
 """
 
-__all__ = ["counting_sort", "counting_sort_sw"]
+__all__ = ["counting_sort"]
 
-
-def counting_sort(a: list, k: int = None) -> list:
-    """Not-in-place counting sort algorithm, which is a stable algorithm, i.e.
-    it maintains the relative order of the same numbers in the sorted list,
-    wrt the unsorted one.
+def counting_sort(a: list,
+                  k: int = None,
+                  key: callable = lambda x: x,
+                  sedgewick_wayne: bool = False) -> list:
+    """Counting sort algorithm, which is a stable algorithm. So, if
+    a[i] == a[j] and i < j, then a[i] still appears before a[j] in the sorted
+    list. It's also not in-place.
 
     Time complexity
 
@@ -287,139 +282,114 @@ def counting_sort(a: list, k: int = None) -> list:
     +-------------+-------------+-------------+
 
     Space complexity: Θ(n + k)."""
-    # We may not need this, but for now I will assume this is the case.
     assert isinstance(a, list)
-    if not all(isinstance(x, int) for x in a):
-        raise TypeError("all elements of a should be integers")
-    if len(a) == 0:
+    assert isinstance(sedgewick_wayne, bool)
+
+    n = len(a)
+
+    # If a is empty, return an empty list, and it does not check the
+    # correctness of any other argument.
+    if n == 0:
         return []
+
+    if not callable(key):
+        raise TypeError("key should be a function")
+
+    try:
+        for x in a:
+            key(x)
+    except Exception:
+        raise KeyError("key is not valid")
+
+    if not all(isinstance(key(x), int) for x in a):
+        raise TypeError("the key attribute of each element of a should be an "
+                        "int")
+
+    # In Sedgewick and Wayne's book, k is denoted by R.
     if not isinstance(k, int):
-        k = max(a) + 1
+        k = key(max(a, key=key)) + 1
+
     if k < 0:
         raise ValueError("k must be greater than or equal to 0")
-    if not all(0 <= x < k for x in a):
-        raise ValueError("all elements of a should be between 0 (included) "
-                         "and k (excluded)")
 
-    # An auxiliary counter list of size k with counters initialized to 0.
-    c = [0] * k
+    if not all(0 <= key(x) < k for x in a):
+        raise ValueError("the key of each element in a should be between 0 "
+                         "(included) and k (excluded)")
 
-    for x in range(len(a)):
-        c[a[x]] += 1
+    # In Sedgewick and Wayne's book, b is denoted by aux.
+    b = [None] * n
 
-    # Now, c[x] contains the number of elements = x.
+    def sw():
+        # This implementation follows closely Sedgewick and Wayne's book.
+        # In Sedgewick and Wayne's book, c is denoted by count.
+        # c[0] will always be 0.
+        c = [0] * (k + 1)
 
-    for x in range(1, k):
-        c[x] += c[x - 1]
+        # Compute frequency counts.
+        for x in range(n):
+            c[key(a[x]) + 1] += 1
 
-    # c[x] now contains the number of elements ≤ x.
+        # Transform counts to indices.
+        for x in range(k):
+            c[x + 1] += c[x]
 
-    # This is a non-in-place algorithm, so we create a list that will hold the
-    # original list sorted.
-    b = [None] * len(a)
+        # Distribute the records.
+        for x in range(n):
+            b[c[key(a[x])]] = a[x]
+            c[key(a[x])] += 1
 
-    # We place each element a[x] into its correct sorted position in b.
-    #
-    # If all n elements are distinct, then, when we first enter the following
-    # line, for each a[x], the value c[a[x]] is the correct final position of
-    # a[x] in the output array, since there are c[a[x]] elements less than or
-    # equal to a[x].
-    #
-    # Because the elements might not be distinct, we decrement c[a[x]] each
-    # time we place a value a[x] into the b array. Decrementing c[a[x]] causes
-    # the next input element with a value equal to a[x], if one exists, to go
-    # to the position immediately before a[x] in b.
-    #
-    # If we started the loop from 0 to len(a) - 1, this implementation would
-    # still produce a sorted list, but it would not be stable. We can easily
-    # see this from the example given in the doc-strings above, where the 4
-    # at index x = 0 of a, i.e. a[x] = a[0] = 0 would be put in the last
-    # position of b, i.e. at position 4, so it would be put after the other
-    # 4 in a, which comes after the first 4. See exercise 8.2-3, p. 196, of
-    # CLRS book.
-    # However, R. Sedgewick and K. Wayne (section 5.1, p. 703, of "Algorithms"
-    # (4th edition)) implement it slightly differently, i.e. starting from
-    # x=0 to x=n-1, but they increment the counter and made other
-    # modifications, and this still makes the algorithm stable. See
-    # "counting_sort_sw" below.
-    # for x in range(0, len(a)):
-    for x in range(len(a) - 1, -1, -1):
-        c[a[x]] -= 1
-        b[c[a[x]]] = a[x]
+    def default():
+        # This implementation is based on
+        # http://opendatastructures.org/ods-java/11_2_Counting_Sort_Radix_So.html.
+
+        # An auxiliary counter list of size k with counters initialized to 0.
+        c = [0] * k
+
+        for x in range(n):
+            c[key(a[x])] += 1
+
+        # Now, c[x] contains the number of elements = x.
+
+        for x in range(1, k):
+            c[x] += c[x - 1]
+
+        # c[x] now contains the number of elements ≤ x.
+
+        # We place each element a[x] into its correct sorted position in b.
+        #
+        # If all n keys are distinct, then, when we first enter the
+        # following line, for each key(a[x]), the value c[key(a[x])] is the
+        # correct final position of key(a[x]) in the output list, since there
+        # are c[key(a[x])] elements less than or equal to a[x].
+        #
+        # Because the elements might not be distinct, we decrement
+        # c[key(a[x])] each time we place a value a[x] into the list b.
+        # Decrementing c[key(a[x])] causes the next input element with a value
+        # equal to a[x], if one exists, to go to the position immediately
+        # before a[x] in b.
+        #
+        # If we started the loop from 0 to n - 1, this implementation
+        # would still produce a sorted list, but it would not be stable. We
+        # can easily see this from the example given in the doc-strings above.
+        # However, R. Sedgewick and K. Wayne (section 5.1, p. 703, of
+        # "Algorithms" (4th edition)) implement it slightly differently. See
+        # the function sw().
+        for x in range(n - 1, -1, -1):
+            c[key(a[x])] -= 1
+            b[c[key(a[x])]] = a[x]
+
+    if sedgewick_wayne:
+        sw()
+    else:
+        default()
 
     return b
-
-
-class KeyIndexedItem:
-
-    # For now, we assume that item is a string, but it could be anything.
-    def __init__(self, item: str, key: int):
-        self.item = item
-        self.key = key
-
-    def __repr__(self):
-        return "{} ({})".format(self.item, self.key)
-
-
-# TODO: test this
-def counting_sort_sw(a: list[KeyIndexedItem], R: int = None,
-                     in_place: bool = True) -> list:
-    """The counting-sort algorithm proposed in the book "Algorithms"
-    (4th edition) by Sedgewick and Wayne, section 5.1 (p. 702), which they
-    call "key-indexed counting".".
-
-    In the book, k is denoted by R.
-
-
-    """
-    if not all(isinstance(x, KeyIndexedItem) for x in a):
-        raise TypeError("all elements of a should be KeyIndexedItem objects")
-    if len(a) == 0:
-        return a
-    N = len(a)
-    # If R is not an int, we compute it based on the keys. Specifically, we
-    # find the maximum key in a, and R = max key + 1.
-    if not isinstance(R, int):
-        max_key = a[0].key
-        for i in range(1, N):
-            if a[i].key > max_key:
-                max_key = a[i].key
-        R = max_key + 1
-    if R < 0:
-        raise ValueError("R must be greater than or equal to 0")
-    if not all(0 <= x.key < R for x in a):
-        raise ValueError("all elements of a should be between 0 (included) "
-                         "and R (excluded)")
-
-    aux = [None] * N
-
-    # count[0] will always be 0.
-    count = [0] * (R + 1)
-
-    # Compute frequency counts.
-    for i in range(N):
-        count[a[i].key + 1] += 1
-
-    # Transform counts to indices.
-    for r in range(R):
-        count[r + 1] += count[r]
-
-    # Distribute the records.
-    for i in range(N):
-        aux[count[a[i].key]] = a[i]
-        count[a[i].key] += 1
-
-    if in_place:
-        # Copy back.
-        for i in range(N):
-            a[i] = aux[i]
-    else:
-        return aux
 
 
 def example1():
     a: list = []
     print(counting_sort(a))
+
     a = [0]
     print(counting_sort(a))
     a = [10, 2]
@@ -437,37 +407,63 @@ def example1():
 
 
 def example2():
+    # Example taken from R. Sedgewick and K. Wayne's book "Algorithms"
+    # (4th edition), section 5.1, p. 703.
     a = [
-        KeyIndexedItem("Anderson", 2),
-        KeyIndexedItem("Brown", 3),
-        KeyIndexedItem("Davis", 3),
-        KeyIndexedItem("Garcia", 4),
-        KeyIndexedItem("Harris", 1),
-        KeyIndexedItem("Jackson", 3),
-        KeyIndexedItem("Johnson", 4),
-        KeyIndexedItem("Jones", 3),
-        KeyIndexedItem("Martin", 1),
-        KeyIndexedItem("Martinez", 2),
-        KeyIndexedItem("Miller", 2),
-        KeyIndexedItem("Moore", 1),
-        KeyIndexedItem("Robinson", 2),
-        KeyIndexedItem("Smith", 4),
-        KeyIndexedItem("Taylor", 3),
-        KeyIndexedItem("Thomas", 4),
-        KeyIndexedItem("Thompson", 4),
-        KeyIndexedItem("White", 2),
-        KeyIndexedItem("Williams", 3),
-        KeyIndexedItem("Wilson", 4),
+        (2, "Anderson"),
+        (3, "Brown"),
+        (3, "Davis"),
+        (4, "Garcia"),
+        (1, "Harris"),
+        (3, "Jackson"),
+        (4, "Johnson"),
+        (3, "Jones"),
+        (1, "Martin"),
+        (2, "Martinez"),
+        (2, "Miller"),
+        (1, "Moore"),
+        (2, "Robinson"),
+        (4, "Smith"),
+        (3, "Taylor"),
+        (4, "Thomas"),
+        (4, "Thompson"),
+        (2, "White"),
+        (3, "Williams"),
+        (4, "Wilson"),
     ]
 
-    b = counting_sort_sw(a, in_place=False)
     from pprint import pprint
+
+    key = lambda x: x[0]
+
+    b = counting_sort(a, key=key)
     pprint(b)
     assert b is not a
-    counting_sort_sw(a)
+
+    pprint(counting_sort(a, key=key))
+
+
+def example3():
+    a = [(1, "no"), (0, "ok"), (1, "yes")]
+    from pprint import pprint
+
+    key = lambda x: x[0]
+    b = counting_sort(a, key=key, sedgewick_wayne=True)
+    pprint(b)
+
+
+def example4():
+    # Raises an exception (as expected).
+    a = [(1, "no"), (0, "ok"), (1, "yes")]
+    from pprint import pprint
+
+    key = lambda x: x[3]
+    b = counting_sort(a, key=key, sedgewick_wayne=True)
     pprint(b)
 
 
 if __name__ == '__main__':
     example1()
     example2()
+    example3()
+    example4()
