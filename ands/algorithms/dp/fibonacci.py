1b7acfec408ba224751232688dbd5c2331f5da7c
Nelson Brochado <nelson.brochado@outlook.com>
2022-05-01 13:49:59 +0200

    docstrings of fibonacci.py updated

--- ands/algorithms/dp/fibonacci.py
+++ ands/algorithms/dp/fibonacci.py
@@ -8,20 +8,47 @@ Author: Nelson Brochado
 
 Created: 20/07/2015
 
-Updated: 07/03/2018
+Updated: 29/03/2022
 
 # Description
 
-In this file you can find some functions that return the nth fibonacci number,
+In this file, you can find some functions that return the nth fibonacci number,
 but they do it in different ways, which has also an impact on the performance
 and asymptotic complexity of the same algorithms.
 
-The Fibonacci numbers is an infinite sequence of numbers, where the next element
-of the sequence is constructed by summing the previous two elements of the same.
+The Fibonacci numbers is an infinite sequence of numbers, where the next
+element of the sequence is constructed by summing the previous two elements of
+the same.
 
-The first two elements are usually 0 and 1, so the next element is 1, so the
-sequence is now {0, 1, 1}. We then add 1 + 1 = 2 two obtain the 4th element of
+The first two elements are (usually) 0 and 1, so the next element is 1, so the
+sequence is now {0, 1, 1}. We then add 1 + 1 = 2 to obtain the 4th element of
 the sequence, which is now {0, 1, 1, 2}, and so on.
+
+The nth Fibonacci number can thus be computed in a recursive way. First, the
+base cases are when n = 0 and n = 1, so fib(0) = 0 and fib(1) = 1. The
+inductive case is fib(n) = fib(n - 1) + fib(n - 2).
+
+It turns out that, if we compute the nth Fibonacci number in this way, we would
+repeat some computations. For example, to compute fib(5), you would need to
+compute fib(4) and fib(3). To compute fib(4), we would need to compute fib(3)
+and fib(2). So, we would compute fib(3) twice.
+
+To solve this problem, once we compute fib(3), we can solve the result. This is
+called memoization.
+
+The time complexity of memoized_fibonacci (below) is linear because we need
+to solve all fib(i), from i=0 to i=n, and each of these sub-problems takes
+constant time.
+
+## References
+
+- "Lecture 19: Dynamic Programming I: Fibonacci, Shortest Paths"
+(https://www.youtube.com/watch?v=OQ5jsbhAv_M&ab_channel=MITOpenCourseWare)
+- https://www.youtube.com/watch?v=P8Xa2BitN3I&ab_channel=HackerRank
+
+## TODO
+
+- Write a Fibonacci function for negative numbers.
 """
 
 __all__ = ["recursive_fibonacci", "memoized_fibonacci", "bottom_up_fibonacci"]
@@ -45,7 +72,7 @@ def _memoized_fibonacci_aux(n: int, memo: dict) -> int:
         return n
     if n not in memo:
         memo[n] = _memoized_fibonacci_aux(n - 1, memo) + \
-                  _memoized_fibonacci_aux(n - 2, memo)
+            _memoized_fibonacci_aux(n - 2, memo)
     return memo[n]
 
 
