57b010cc2f15ca76a2e3b8212688f1c5628c78e2
Nelson Brochado <nelson.brochado@outlook.com>
2022-05-01 17:00:42 +0200

    default value of the parameter 'method_name' of the test classes for sorting algorithms updated

--- tests/algorithms/sorting/test_merge_sort.py
+++ tests/algorithms/sorting/test_merge_sort.py
@@ -9,6 +9,6 @@ from tests.algorithms.sorting.base_tests import *
 
 
 class TestMergeSort(unittest.TestCase, SortingAlgorithmTests):
-    def __init__(self, method_name="runTest"):
+    def __init__(self, method_name="__init__"):
         unittest.TestCase.__init__(self, method_name)
         SortingAlgorithmTests.__init__(self, merge_sort, False)
