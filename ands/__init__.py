d1a2f6b43db5310004432d17844cffd92c180e10
Nelson Brochado <nelson.brochado@outlook.com>
2022-05-03 00:13:10 +0200

    multiple __init__.py files updated

--- ands/__init__.py
+++ ands/__init__.py
@@ -1,9 +1,4 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# File to allow this directory to be treated as a python package.
-
-# From Python 3.3+ this way of constructing subpackages
-# should no more be necessary, but for some reason,
-# at the moment of this writing, pdoc does not seem to work
-# as expected if I omit these __init__.py files.
+# File to allow this directory to be treated as a Python package.
