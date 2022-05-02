fcdbe02dcfc930a435caa74996ac4f2325be4185
Nelson Brochado <nelson.brochado@outlook.com>
2022-05-03 01:33:21 +0200

    base_tests.py in the sorting package updated

--- tests/algorithms/sorting/base_tests.py
+++ tests/algorithms/sorting/base_tests.py
@@ -15,7 +15,6 @@ Updated: 04/08/2018
 Common unit tests for the functions to sort sequences.
 """
 
-
 from random import randint
 
 from ands.algorithms.recursion.is_sorted import iterative_is_sorted
@@ -29,9 +28,15 @@ def build_random_list(size=10, start=-10, end=10):
 
 
 class SortingAlgorithmTests:
-    def __init__(self, sorting_algorithm, in_place):
+    def __init__(self, sorting_algorithm, in_place, start=randint(-10001, -1),
+                 end=randint(0, 10000)):
         self.sorting_algorithm = sorting_algorithm
         self.in_place = in_place
+
+        # the default smallest and biggest value in the lists that are used
+        # to test the sorting algorithms.
+        self.start = start
+        self.end = end
 
     def assert_commonalities(self, a):
         b = self.sorting_algorithm(a)
@@ -46,14 +51,14 @@ class SortingAlgorithmTests:
         self.assert_commonalities([])
 
     def test_size_1(self):
-        a = build_random_list(1)
+        a = build_random_list(size=1, start=self.start, end=self.end)
         self.assert_commonalities(a)
 
     def test_size_2(self):
-        a = build_random_list(2)
+        a = build_random_list(size=2, start=self.start, end=self.end)
         self.assert_commonalities(a)
 
     def test_random_size(self):
         size = randint(3, 1113)
-        a = build_random_list(size, -432, 1233)
+        a = build_random_list(size=size, start=self.start, end=self.end)
         self.assert_commonalities(a)
